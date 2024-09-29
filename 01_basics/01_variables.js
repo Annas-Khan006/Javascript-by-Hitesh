900792924ad14074b5e87e496f6195eec9e4a6f1
Annas-khan006 <khanannas006@gmail.com>
2024-09-29 23:25:23 +0500

    3 files done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+// const accountId = 144542
+// let accountEmail = "annas@gmail.com"
+// var accountPassword = "12345678"
+// accountCity = "Bahaw/alpur"
+// let accountState;
+
+// accountId = 2 // not allowed
+
+// accountEmail = "ak@.com"
+// accountPassword = "212121"/
+// accountCity = "Multan"
+
+// console.log(accountId);
+
+/*
+    prefer not to use var 
+    because of issue in block scope and functional scope
+*/
+
+// console.table([
+    // accountId,
+    // accountEmail,
+    // accountPassword,
+    // accountCity,
+    // accountState
+// ])
