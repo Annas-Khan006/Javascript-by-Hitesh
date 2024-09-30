34886616e5c54c77696a737e5c24e75a7e8d5218
Annas-khan006 <khanannas006@gmail.com>
2024-09-30 12:54:09 +0500

    4 files of js is done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+
+// console.log("2" === 2);
+
+
+
+// ************ Summary ************
+// null >,<,== 0, or undefined >,<,== 0 
+// is trh ky conversion apko kabhi bhi confusion mn dal skty hn
